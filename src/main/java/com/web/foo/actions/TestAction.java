02e70829d325759babacb0d3adc5e6b9c7ce4f9f
周绍禹 <hzzhoushaoyu@corp.netease.com>
2013-03-15 20:18:18 +0800

    添加struts

---
+++ src/main/java/com/web/foo/actions/TestAction.java
@@ -0,0 +1,17 @@
+package com.web.foo.actions;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class TestAction extends ActionSupport {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -667365494746867929L;
+
+	@Override
+	public String execute() throws Exception {
+		return super.execute();
+	}
+	
+}
---
+++ src/test/java/com/web/foo/beans/TestTest.java
@@ -0,0 +1,11 @@
+package com.web.foo.beans;
+
+import junit.framework.TestCase;
+
+public class TestTest extends TestCase {
+
+	public void testTrue(){
+		this.assertTrue(true);
+	}
+	
+}
