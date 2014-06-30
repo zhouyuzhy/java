a1f338e74b727e65d2443ff889138951bd1264c4
周绍禹 <hzzhoushaoyu@corp.netease.com>
2014-06-30 23:13:31 +0800

    五人足球赛

---
+++ src/main/java/org/test/httpclient/Football.java
@@ -0,0 +1,183 @@
+package org.test.httpclient;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.zip.GZIPInputStream;
+
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.HttpException;
+import org.apache.commons.httpclient.HttpMethod;
+import org.apache.commons.httpclient.methods.GetMethod;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.math.NumberUtils;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+public class Football
+{
+
+	public static final String COOKIE = "vjuids=-36eaccbd9.13fd8b62a15.0.e1a838ba; ALLYESID4=00130714092738258523247; usertrack=ezq0JFI63VFkTm4qJbtmAg==; NTES_REPLY_NICKNAME=zhouyuzhy%40126.com%7Czhouyuzhy126%7C-4396741636047328574%7C6037906236%7Chttp%3A%2F%2Fmimg.126.net%2Fp%2Fbutter%2F1008031648%2Fimg%2Fface_big.gif%7C%7C1%7C2; pgv_pvi=6712780800; _ntes_nuid=58f255d303d2b7d12cb143a7c655d9d5; _ntes_nnid=58f255d303d2b7d12cb143a7c655d9d5,1402832271270; hasPay=0; locOfHebeiSite=others; ancmt=1; ne_analysis_trace_id=1404131640571; vinfo_n_f_l_n3=1ed5cd37e9f3ce62.1.11.1394935693000.1403786424649.1404131647230; s_n_f_l_n3=1ed5cd37e9f3ce621403790295216; pgr_n_f_l_n3=1ed5cd37e9f3ce62140413164057463; nteslogger_exit_time=1404131647237; JSESSIONID=D8F56F229F031F8A9BAA6A37EC28E3E1.app-1-8010; NTES_SESS=Fuw._i4R1meZp8GGsWvmVW9hCvaeB95zetsezgPJ3NagRqGuuNlbWp5uElKFPhGxpKHVNywXy5kjcNJe6s_M.gZR8693K3MJ.L97lZXU4WQW_3czDszSJ5WWP3ZjgmRA4Cwr4fcRn_mv9pK1LmD20.9HSt.zgYf3G_YqmDo4EQ0braQncatKSo.WV; S_INFO=1404131668|0|#3&100#|zhouyuzhy@126.com#zhoushaoyuzsy#huangjqi@126.com#qsampt@yeah.net; P_INFO=zhouyuzhy@126.com|1404131668|1|other|11&15|zhj&1404131535&exchange#zhj&330100#10#0#0|151496&0|xym_client&caipiao&epay|zhouyuzhy@126.com; NTES_PASSPORT=DTz_tFz_2S_10CDalDSjpa5RlQddzg.Q5yP9rhVuOzs6wkMppI5NvlWph5BDV4MilBqcIgPKgWy483zV9S2pYK67gwhPBmtHrGgdqffIOfwt1A2dVSq50Vrvc; ANTICSRF=99e97e86b2b713c2d82ef3c9e004c792; Futsal_SID=59ffe2c0-6cf2-452c-961d-c7a2090c3910; sv_id=50; vjlast=1373730384.1404131641.11; _ga=GA1.2.80161961.1403615257; AJSTAT_ok_pages=9; AJSTAT_ok_times=10";
+	public static final boolean challenge100 = true;//false即是挑战比在自己前面名次的任意名次
+
+	/**
+	 * @param args中文
+	 * @throws IOException 
+	 * @throws HttpException 
+	 * @throws InterruptedException 
+	 */
+	public static void main(String[] args) throws HttpException, IOException, InterruptedException
+	{
+		HttpClient client = new HttpClient();
+		while (true)
+		{
+			try
+			{
+				int t = tili();
+				if (t <= 0)
+				{
+					System.err.println("no energy");
+					continue;
+				}
+				String sb = httpRequest("http://a.wc.163.com/game/ranking?showHis=true&_=1404131666549");
+				analysisHTML(sb);
+				System.out.println("挑战完成");
+			}
+			catch (IllegalStateException e)
+			{
+				System.err.println(e.getMessage());
+			}
+			catch (Exception e)
+			{
+				e.printStackTrace();
+			}
+			finally
+			{
+				Thread.sleep(60 * 1000);
+			}
+		}
+	}
+
+	private static Integer tili() throws HttpException, IOException
+	{
+		Document doc = Jsoup.parse(httpRequest("http://a.wc.163.com/history?showHis=true&_=1404138464724"));
+		for (Element ele : doc.getElementsByClass("lifes-txt"))
+		{
+			return NumberUtils.createInteger(String.valueOf((ele.text().charAt(ele.text().length() - 1))));
+		}
+		return -1;
+	}
+
+	/**
+	 * @param method
+	 */
+	private static void httpHeads(HttpMethod method)
+	{
+		method.setRequestHeader("Accept", "text/html, */*; q=0.01");
+		method.setRequestHeader(
+				"User-Agent",
+				"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/29.0.1547.66 Safari/537.36 LBBROWSER");
+		method.setRequestHeader("Referer", "http://a.wc.163.com/main");
+		method.setRequestHeader("Accept-Encoding", "gzip,deflate,sdch");
+		method.setRequestHeader("Accept-Language", "zh-CN,zh;q=0.8");
+		method.setRequestHeader("Cookie", COOKIE);
+	}
+
+	/**
+	 * 01 05 13 26 28 32
+	 *	09 15
+	 * @param html
+	 * @throws IOException 
+	 * @throws HttpException 
+	 */
+	public static void analysisHTML(String html) throws HttpException, IOException
+	{
+		Document doc = Jsoup.parse(html);
+		//获取100名内的战斗url
+		String url = call(doc);
+		//开始比赛
+		httpRequest(url);
+	}
+
+	/**
+	 * @param url
+	 * @throws IOException
+	 * @throws HttpException
+	 */
+	private static String httpRequest(String url) throws IOException, HttpException
+	{
+		HttpMethod method = new GetMethod(url);
+		httpHeads(method);
+		HttpClient client = new HttpClient();
+		int status = client.executeMethod(method);
+		if (status == 200)
+		{
+			InputStream is = method.getResponseBodyAsStream();
+			GZIPInputStream gzipIs = new GZIPInputStream(is);
+			BufferedReader br = new BufferedReader(new InputStreamReader(gzipIs));
+			StringBuffer sb = new StringBuffer();
+			String line = null;
+			while ((line = br.readLine()) != null)
+			{
+				sb.append(line).append("\n");
+			}
+			is.close();
+			method.releaseConnection();
+			return sb.toString();
+		}
+		return "";
+	}
+
+	private static String call(Document doc) throws HttpException, IOException
+	{
+		Long myRank = getMyRank();
+		if (myRank != null && myRank < 100)
+		{
+			throw new IllegalStateException("都在100名了，没必要打了！！！等一分钟再检测");
+		}
+		for (Element div : doc.getElementsByAttributeValue("class", "user-team-rank"))
+		{
+			if (!StringUtils.equals(div.tagName(), "div"))
+			{
+				continue;
+			}
+			Long rank = NumberUtils.createLong(div.text());
+			if (rank == null)
+			{
+				continue;
+			}
+			else if (challenge100 && rank >= 100)
+			{
+				continue;
+			}
+			else if (!challenge100 && (myRank == null || rank >= myRank))
+			{
+				if (myRank == null)
+				{
+					System.err.println("my rank is null");
+				}
+				continue;
+			}
+			System.out.println("准备挑战:" + rank);
+			return div.nextElementSibling().attr("href").replace("#", "http://a.wc.163.com/game");
+		}
+		return null;
+	}
+
+	private static Long getMyRank() throws HttpException, IOException
+	{
+		String url = "http://a.wc.163.com/main";
+		String r = httpRequest(url);
+		Document doc = Jsoup.parse(r);
+		Elements eles = doc.getElementsByClass("myteam");
+		for (Element ele : eles)
+		{
+			return NumberUtils.createLong((ele.child(0).child(1).child(0).text()));
+		}
+		return null;
+	}
+
+}
